ed693afe316ed069f2aadaff71a60ac485281794
maxvyshnianchyn <50170270+maxvyshnianchyn@users.noreply.github.com>
2020-03-22 13:29:03 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+
+/*var color = ['red', 'blue', 'violet' ,'green', 'yellow']
+var i = 0
+document.querySelector('button').addEventListener('click', onClick)
+function onClick(){
+    i = i < color.length ? ++i : 0;
+    document.querySelector('body').style.backgroundColor = color[i];
+}
+*/
